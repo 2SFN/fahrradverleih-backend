0cd40f4f5bffc199cbc55384eee02510cd6186e1
SFN <service.sfn+bitbucket@gmail.com>
2022-08-22 14:20:36 +0200

    + Unterstützung für SSL-Verbindungen.

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,17 @@ Einfaches Backend, das eine RESTful API für den Fahrradverleih zur
 Verfügung stellt. Daten werden nicht persistiert und es sind keine
 Sicherheits-Features implementiert.
 
-Standardmäßiger Port für den WebServer: 3000
+Standardmäßiger Port für den WebServer: 3000/3443 (s. .env.example)
 */
 import express from 'express';
 import cors from 'cors';
 import {CorsOptions} from "cors";
 import router from "./api";
+import * as https from "https";
 
 ///////////////  Express & Umgebung  ///////////////
-const server = express();
-server.use(express.json());
+const app = express();
+app.use(express.json());
 
 // Konfiguration für CORS innerhalb der Ionic-Entwicklungsumgebung
 // s. https://ionicframework.com/docs/troubleshooting/cors
@@ -30,17 +31,26 @@ const corsOptions: CorsOptions = {
     origin: allowedOrigins
 }
 
-server.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // Auf Port hören
-const {
-    PORT = 3000,
-} = process.env;
+const {PORT = 3000} = process.env;
 
-server.listen(PORT, () => {
-    console.log(`Server listenening on port ${PORT}!`);
+app.listen(PORT, () => {
+    console.log(`Server listening (HTTP) Port: ${PORT}`);
 });
 
 // API-Endpunkte (router) einbinden
-server.use('/api', router);
+app.use('/api', router);
 
+// Optional: API auch mit SSL-Unterstützung zur Verfügung stellen
+if (process.env.ENABLE_SSL === "yes") {
+    const secureServer = https.createServer({
+        key: process.env.SSL_KEY,
+        cert: process.env.SSL_CRT
+    }, app);
+    const {SSL_PORT = 3443} = process.env;
+    secureServer.listen(SSL_PORT, () => {
+        console.log(`Server listening (HTTPS) Port: ${SSL_PORT}`);
+    });
+}
